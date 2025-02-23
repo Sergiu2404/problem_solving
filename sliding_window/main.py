b3247d686e3f80c51c9f804cb6e03a8b912d6519
Goian-Sergiu <sergiu_goian@yahoo.com>
2025-02-23 17:54:33 +0200

    sliding window easy

--- sliding_window/main.py
+++ sliding_window/main.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def brute_force(interval_size, array):
     i = 0
     curr_max = float('-inf')
@@ -36,4 +39,35 @@ def sliding_window(window_size, array):
 
     return curr_max
 
-print(sliding_window(4, [1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 10]))
+#print(sliding_window(4, [1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 10]))
+
+
+
+
+#BEST TIME TO BUY AND SELL STOCK
+# You are given an integer array prices where prices[i] is the price of NeetCoin on the ith day.
+# You may choose a single day to buy one NeetCoin and choose a different day in the future to sell it.
+# Return the maximum profit you can achieve. You may choose to not make any transactions, in which case the profit would be 0.
+# Ex 1:
+# Input: prices = [10,1,5,6,7,1]
+# Output: 6
+# Explanation: Buy prices[1] and sell prices[4], profit = 7 - 1 = 6.
+def maxProfit(prices: List[int]) -> int:
+    if sorted(prices, reverse=True) == prices:
+        return 0
+
+    start = 0
+    end = 1
+
+    profit = 0
+    while end < len(prices):
+        if prices[start] < prices[end]:
+            profit = max(prices[end] - prices[start], profit)
+        else:
+            start = end
+
+        end += 1
+
+    return profit
+
+print(maxProfit([10,1,5,6,7,1]))
